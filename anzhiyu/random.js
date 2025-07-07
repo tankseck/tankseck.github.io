cd9f78608fbf32d25540505e99c23ba81f6b6e19
tankseck <202078893@qq.com>
2025-07-07 15:36:26 +0800

    Site updated: 2025-07-07 15:36:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/06/hello-world/","2025/07/06/test1/"];function toRandomPost(){
+var posts=["2025/07/06/hello-world/","2025/07/06/test1/","2025/07/07/test2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
